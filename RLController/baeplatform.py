57ae311c571fc560cf4076a0c207f0639a650fbc
ruizhang2015 <wfzrsjtu@126.com>
2014-09-01 11:29:38 +0800

    RLController

---
+++ RLController/baeplatform.py
@@ -0,0 +1,368 @@
+import random
+import sys
+import mdp
+import environment
+import util
+import optparse
+import time
+
+class BaePlatform(mdp.MarkovDecisionProcess):
+  """
+    BaePlatform
+  """
+  def __init__(self):
+    # layout
+    #if type(grid) == type([]): grid = makeGrid(grid)
+    #self.grid = grid
+    
+    # parameters
+    self.livingReward = 0.0
+    self.noise = 0.0
+        
+  def setNoise(self, noise):
+    """
+    The probability of moving in an unintended direction.
+    """
+    self.noise = noise
+        
+                                    
+  def getPossibleActions(self, state):
+    """
+    Returns list of valid actions for 'state'.
+    
+    Note that you can request moves into walls and
+    that "exit" states transition to the terminal
+    state under the special action "done".
+    """
+    #if state == self.grid.terminalState:
+     # return ()
+    ucpu,umem,nvins = state
+    if ucpu == 2 and umem == 2 and nvins == 3:
+      return ('nop','move','dech','decv')
+    if ucpu == 2 or umem == 2 or nvins == 3:
+      return ('nop','move','dech','decv')
+    return ('nop',)
+    
+  def getStates(self):
+    """
+    Return list of all states.
+    """
+    # The true terminal state.
+    states = []
+    for x in range(3):
+      for y in range(3):
+        for z in range(4):
+          state = (x,y,z)
+          states.append(state)
+    return states
+        
+  def getReward(self, state, action, nextState):
+    """
+    Get reward for state, action, nextState transition.
+    
+    Note that the reward depends only on the state being
+    departed (as in the R+N book examples, which more or
+    less use this convention).
+    """
+    ucpu,umem,nvins = state
+    nucpu,numem,nnvins = nextState
+    reward = (nnvins - nvins) * 5
+    if nucpu == 1 and ucpu != 1: 
+      reward += 1
+    if numem == 1 and umem != 1: 
+      reward += 1
+    if ucpu == 1 and nucpu != 1: 
+      reward -= 1
+    if umem == 1 and numem != 1: 
+      reward -= 1
+    
+    return reward
+        
+  def getState(self):
+    # TO DO: get state from monitor
+    state = (0,0,1)
+    return state
+       
+class BaeplatformEnvironment(environment.Environment):
+    
+  def __init__(self, baePlatform):
+    self.baeplatform = baePlatform
+    self.reset()
+            
+  def getCurrentState(self):
+    return self.state
+        
+  def getPossibleActions(self, state):        
+    return self.baeplatform.getPossibleActions(state)
+        
+  def doAction(self, action):
+    state = self.getCurrentState()
+    # TO DO: execute the action and get nextState
+    nextState = (1,1,0)
+    reward = self.baePlatform.getReward(state, action, nextState)
+    self.state = nextState
+    return (nextState, reward)
+        
+  def reset(self):
+    self.state = self.baePlatform.getStartState()
+
+def getUserAction(state, actionFunction):
+  """
+  Get an action from the user (rather than the agent).
+  
+  Used for debugging and lecture demos.
+  """
+  import graphicsUtils
+  action = None
+  while True:
+    keys = graphicsUtils.wait_for_keys()
+    if 'Up' in keys: action = 'north'
+    if 'Down' in keys: action = 'south'
+    if 'Left' in keys: action = 'west'
+    if 'Right' in keys: action = 'east'
+    if 'q' in keys: sys.exit(0)
+    if action == None: continue
+    break
+  actions = actionFunction(state)
+  if action not in actions:
+    action = actions[0]
+  return action
+
+def runEpisode(agent, environment, discount, decision, display, message, pause, episode):
+  returns = 0
+  totalDiscount = 1.0
+  environment.reset()
+  if 'startEpisode' in dir(agent): agent.startEpisode()
+  message("BEGINNING EPISODE: "+str(episode)+"\n")
+  while True:
+
+    # DISPLAY CURRENT STATE
+    state = environment.getCurrentState()
+    print(state)
+    pause()
+    
+    # END IF IN A TERMINAL STATE
+    actions = environment.getPossibleActions(state)
+    if len(actions) == 0:
+      message("EPISODE "+str(episode)+" COMPLETE: RETURN WAS "+str(returns)+"\n")
+      return returns
+    
+    # GET ACTION (USUALLY FROM AGENT)
+    action = decision(state)
+    if action == None:
+      raise 'Error: Agent returned None action'
+    
+    # EXECUTE ACTION
+    nextState, reward = environment.doAction(action)
+    message("Started in state: "+str(state)+
+            "\nTook action: "+str(action)+
+            "\nEnded in state: "+str(nextState)+
+            "\nGot reward: "+str(reward)+"\n")    
+    # UPDATE LEARNER
+    if 'observeTransition' in dir(agent): 
+        agent.observeTransition(state, action, nextState, reward)
+    
+    returns += reward * totalDiscount
+    totalDiscount *= discount
+
+  if 'stopEpisode' in dir(agent):
+    agent.stopEpisode()
+
+def parseOptions():
+    optParser = optparse.OptionParser()
+    optParser.add_option('-d', '--discount',action='store',
+                         type='float',dest='discount',default=0.9,
+                         help='Discount on future (default %default)')
+    optParser.add_option('-r', '--livingReward',action='store',
+                         type='float',dest='livingReward',default=0.0,
+                         metavar="R", help='Reward for living for a time step (default %default)')
+    optParser.add_option('-n', '--noise',action='store',
+                         type='float',dest='noise',default=0.2,
+                         metavar="P", help='How often action results in ' +
+                         'unintended direction (default %default)' )
+    optParser.add_option('-e', '--epsilon',action='store',
+                         type='float',dest='epsilon',default=0.3,
+                         metavar="E", help='Chance of taking a random action in q-learning (default %default)')
+    optParser.add_option('-l', '--learningRate',action='store',
+                         type='float',dest='learningRate',default=0.5,
+                         metavar="P", help='TD learning rate (default %default)' )
+    optParser.add_option('-i', '--iterations',action='store',
+                         type='int',dest='iters',default=10,
+                         metavar="K", help='Number of rounds of value iteration (default %default)')
+    optParser.add_option('-k', '--episodes',action='store',
+                         type='int',dest='episodes',default=1,
+                         metavar="K", help='Number of epsiodes of the MDP to run (default %default)')
+    optParser.add_option('-g', '--grid',action='store',
+                         metavar="G", type='string',dest='grid',default="BookGrid",
+                         help='Grid to use (case sensitive; options are BookGrid, BridgeGrid, CliffGrid, MazeGrid, default %default)' )
+    optParser.add_option('-w', '--windowSize', metavar="X", type='int',dest='gridSize',default=150,
+                         help='Request a window width of X pixels *per grid cell* (default %default)')
+    optParser.add_option('-a', '--agent',action='store', metavar="A",
+                         type='string',dest='agent',default="random",
+                         help='Agent type (options are \'random\', \'value\' and \'q\', default %default)')
+    optParser.add_option('-t', '--text',action='store_true',
+                         dest='textDisplay',default=False,
+                         help='Use text-only ASCII display')
+    optParser.add_option('-p', '--pause',action='store_true',
+                         dest='pause',default=False,
+                         help='Pause GUI after each time step when running the MDP')
+    optParser.add_option('-q', '--quiet',action='store_true',
+                         dest='quiet',default=False,
+                         help='Skip display of any learning episodes')
+    optParser.add_option('-s', '--speed',action='store', metavar="S", type=float,
+                         dest='speed',default=1.0,
+                         help='Speed of animation, S > 1.0 is faster, 0.0 < S < 1.0 is slower (default %default)')
+    optParser.add_option('-m', '--manual',action='store_true',
+                         dest='manual',default=False,
+                         help='Manually control agent')
+    optParser.add_option('-v', '--valueSteps',action='store_true' ,default=False,
+                         help='Display each step of value iteration')
+
+    opts, args = optParser.parse_args()
+    
+    if opts.manual and opts.agent != 'q':
+      print '## Disabling Agents in Manual Mode (-m) ##'
+      opts.agent = None
+
+    # MANAGE CONFLICTS
+    if opts.textDisplay or opts.quiet:
+    # if opts.quiet:      
+      opts.pause = False
+      # opts.manual = False
+      
+    if opts.manual:
+      opts.pause = True
+      
+    return opts
+
+  
+if __name__ == '__main__':
+  
+  opts = parseOptions()
+
+  ###########################
+  # GET THE GRIDWORLD
+  ###########################
+
+  import baeplatform
+  #mdpFunction = getattr(gridworld, "get"+opts.grid)
+  #mdp = mdpFunction()
+  #mdp.setLivingReward(opts.livingReward)
+  #mdp.setNoise(opts.noise)
+  env = baeplatform.BaeplatformEnvironment()
+
+  ###########################
+  # GET THE DISPLAY ADAPTER
+  ###########################
+
+  import textGridworldDisplay
+  display = textGridworldDisplay.TextGridworldDisplay(mdp)
+  if not opts.textDisplay:
+    import graphicsGridworldDisplay
+    display = graphicsGridworldDisplay.GraphicsGridworldDisplay(mdp, opts.gridSize, opts.speed)
+  display.start()
+
+  ###########################
+  # GET THE AGENT
+  ###########################
+
+  import valueIterationAgents, qlearningAgents
+  a = None
+  if opts.agent == 'value':
+    a = valueIterationAgents.ValueIterationAgent(mdp, opts.discount, opts.iters)
+  elif opts.agent == 'q':
+    #env.getPossibleActions, opts.discount, opts.learningRate, opts.epsilon
+    #simulationFn = lambda agent, state: simulation.GridworldSimulation(agent,state,mdp)
+    gridWorldEnv = GridworldEnvironment(mdp)
+    actionFn = lambda state: mdp.getPossibleActions(state)
+    qLearnOpts = {'gamma': opts.discount, 
+                  'alpha': opts.learningRate, 
+                  'epsilon': opts.epsilon,
+                  'actionFn': actionFn}
+    a = qlearningAgents.QLearningAgent(**qLearnOpts)
+  elif opts.agent == 'random':
+    # # No reason to use the random agent without episodes
+    if opts.episodes == 0:
+      opts.episodes = 10
+    class RandomAgent:
+      def getAction(self, state):
+        return random.choice(mdp.getPossibleActions(state))
+      def getValue(self, state):
+        return 0.0
+      def getQValue(self, state, action):
+        return 0.0
+      def getPolicy(self, state):
+        "NOTE: 'random' is a special policy value; don't use it in your code."
+        return 'random'
+      def update(self, state, action, nextState, reward):
+        pass      
+    a = RandomAgent()
+  else:
+    if not opts.manual: raise 'Unknown agent type: '+opts.agent
+    
+    
+  ###########################
+  # RUN EPISODES
+  ###########################
+  # DISPLAY Q/V VALUES BEFORE SIMULATION OF EPISODES
+  if not opts.manual and opts.agent == 'value':
+    if opts.valueSteps:
+      for i in range(opts.iters):
+        tempAgent = valueIterationAgents.ValueIterationAgent(mdp, opts.discount, i)
+        display.displayValues(tempAgent, message = "VALUES AFTER "+str(i)+" ITERATIONS")
+        display.pause()        
+    
+    display.displayValues(a, message = "VALUES AFTER "+str(opts.iters)+" ITERATIONS")
+    display.pause()
+    display.displayQValues(a, message = "Q-VALUES AFTER "+str(opts.iters)+" ITERATIONS")
+    display.pause()
+    
+  
+
+  # FIGURE OUT WHAT TO DISPLAY EACH TIME STEP (IF ANYTHING)
+  displayCallback = lambda x: None
+  if not opts.quiet:
+    if opts.manual and opts.agent == None: 
+      displayCallback = lambda state: display.displayNullValues(state)
+    else:
+      if opts.agent == 'random': displayCallback = lambda state: display.displayValues(a, state, "CURRENT VALUES")
+      if opts.agent == 'value': displayCallback = lambda state: display.displayValues(a, state, "CURRENT VALUES")
+      if opts.agent == 'q': displayCallback = lambda state: display.displayQValues(a, state, "CURRENT Q-VALUES")
+
+  messageCallback = lambda x: printString(x)
+  if opts.quiet:
+    messageCallback = lambda x: None
+
+  # FIGURE OUT WHETHER TO WAIT FOR A KEY PRESS AFTER EACH TIME STEP
+  pauseCallback = lambda : None
+  if opts.pause:
+    pauseCallback = lambda : display.pause()
+
+  # FIGURE OUT WHETHER THE USER WANTS MANUAL CONTROL (FOR DEBUGGING AND DEMOS)  
+  if opts.manual:
+    decisionCallback = lambda state : getUserAction(state, mdp.getPossibleActions)
+  else:
+    decisionCallback = a.getAction  
+    
+  # RUN EPISODES
+  if opts.episodes > 0:
+    print
+    print "RUNNING", opts.episodes, "EPISODES"
+    print
+  returns = 0
+  for episode in range(1, opts.episodes+1):
+    returns += runEpisode(a, env, opts.discount, decisionCallback, displayCallback, messageCallback, pauseCallback, episode)
+  if opts.episodes > 0:
+    print
+    print "AVERAGE RETURNS FROM START STATE: "+str((returns+0.0) / opts.episodes)
+    print 
+    print
+    
+  # DISPLAY POST-LEARNING VALUES / Q-VALUES
+  if opts.agent == 'q' and not opts.manual:
+    display.displayQValues(a, message = "Q-VALUES AFTER "+str(opts.episodes)+" EPISODES")
+    display.pause()
+    display.displayValues(a, message = "VALUES AFTER "+str(opts.episodes)+" EPISODES")
+    display.pause()
+    
+   
---
+++ RLController/qlearningAgents.py
@@ -0,0 +1,196 @@
+from game import *
+from learningAgents import ReinforcementAgent
+from featureExtractors import *
+
+import random,util,math
+          
+class QLearningAgent(ReinforcementAgent):
+  """
+    Q-Learning Agent
+    
+    Functions you should fill in:
+      - getQValue
+      - getAction
+      - getValue
+      - getPolicy
+      - update
+      
+    Instance variables you have access to
+      - self.epsilon (exploration prob)
+      - self.alpha (learning rate)
+      - self.gamma (discount rate)
+    
+    Functions you should use
+      - self.getLegalActions(state) 
+        which returns legal actions
+        for a state
+  """
+  def __init__(self, **args):
+    "You can initialize Q-values here..."
+    ReinforcementAgent.__init__(self, **args)
+
+    "*** YOUR CODE HERE ***"
+    self.qvalues = util.Counter()
+
+  
+  def getQValue(self, state, action):
+    """
+      Returns Q(state,action)    
+      Should return 0.0 if we never seen
+      a state or (state,action) tuple 
+    """
+    "*** YOUR CODE HERE ***"
+    return self.qvalues[state, action]
+  
+    
+  def getValue(self, state):
+    """
+      Returns max_action Q(state,action)        
+      where the max is over legal actions.  Note that if
+      there are no legal actions, which is the case at the
+      terminal state, you should return a value of 0.0.
+    """
+    "*** YOUR CODE HERE ***"
+    actions = self.getLegalActions(state)
+    if (not actions):
+        return 0.0
+    value = -1000
+    for action in actions:
+      qvalue = self.getQValue(state, action)
+      if (value < qvalue):
+        value = qvalue
+    return value
+    
+  def getPolicy(self, state):
+    """
+      Compute the best action to take in a state.  Note that if there
+      are no legal actions, which is the case at the terminal state,
+      you should return None.
+    """
+    "*** YOUR CODE HERE ***"
+    legalActions = self.getLegalActions(state)
+    if (not legalActions):
+      return None
+    maxqvalue = -1000
+    for action in legalActions:
+      qvalue = self.qvalues[state, action]
+      if (maxqvalue < qvalue):
+        maxqvalue = qvalue
+        optact = action
+    return optact
+
+  def getAction(self, state):
+    """
+      Compute the action to take in the current state.  With
+      probability self.epsilon, we should take a random action and
+      take the best policy action otherwise.  Note that if there are
+      no legal actions, which is the case at the terminal state, you
+      should choose None as the action.
+    
+      HINT: You might want to use util.flipCoin(prob)
+      HINT: To pick randomly from a list, use random.choice(list)
+    """  
+    # Pick Action
+    legalActions = self.getLegalActions(state)
+    #action = None
+    "*** YOUR CODE HERE ***"
+    if (not legalActions):
+        return None
+    if (util.flipCoin(self.epsilon)):
+        return random.choice(legalActions)
+    
+    return self.getPolicy(state)
+  
+  def update(self, state, action, nextState, reward):
+    """
+      The parent class calls this to observe a 
+      state = action => nextState and reward transition.
+      You should do your Q-Value update here
+      
+      NOTE: You should never call this function,
+      it will be called on your behalf
+    """
+    "*** YOUR CODE HERE ***"
+    self.qvalues[state, action] = (1 - self.alpha) * self.qvalues[state, action] 
+    sample = reward + self.gamma * self.getValue(nextState)
+    self.qvalues[state, action] += self.alpha * sample
+    
+    
+class PacmanQAgent(QLearningAgent):
+  "Exactly the same as QLearningAgent, but with different default parameters"
+  
+  def __init__(self, epsilon=0.05,gamma=0.8,alpha=0.2, numTraining=0, **args):
+    """
+    These default parameters can be changed from the pacman.py command line.
+    For example, to change the exploration rate, try:
+        python pacman.py -p PacmanQLearningAgent -a epsilon=0.1
+    
+    alpha    - learning rate
+    epsilon  - exploration rate
+    gamma    - discount factor
+    numTraining - number of training episodes, i.e. no learning after these many episodes
+    """
+    args['epsilon'] = epsilon
+    args['gamma'] = gamma
+    args['alpha'] = alpha
+    args['numTraining'] = numTraining
+    QLearningAgent.__init__(self, **args)
+
+  def getAction(self, state):
+    """
+    Simply calls the getAction method of QLearningAgent and then
+    informs parent of action for Pacman.  Do not change or remove this
+    method.
+    """
+    action = QLearningAgent.getAction(self,state)
+    self.doAction(state,action)
+    return action
+
+    
+class ApproximateQAgent(PacmanQAgent):
+  """
+     ApproximateQLearningAgent
+     
+     You should only have to overwrite getQValue
+     and update.  All other QLearningAgent functions
+     should work as is.
+  """
+  def __init__(self, extractor='IdentityExtractor', **args):
+    self.featExtractor = util.lookup(extractor, globals())()
+    PacmanQAgent.__init__(self, **args)
+
+    # You might want to initialize weights here.
+    "*** YOUR CODE HERE ***"
+    self.w = util.Counter()
+    
+  def getQValue(self, state, action):
+    """
+      Should return Q(state,action) = w * featureVector
+      where * is the dotProduct operator
+    """
+    "*** YOUR CODE HERE ***"
+    feats = self.featExtractor.getFeatures(state, action)
+    self.qvalues[state, action] = self.w * feats
+    return self.qvalues[state, action]
+    
+  def update(self, state, action, nextState, reward):
+    """
+       Should update your weights based on transition  
+    """
+    "*** YOUR CODE HERE ***"
+    diff = reward + self.gamma * self.getValue(nextState) - self.getQValue(state, action)
+    feats = self.featExtractor.getFeatures(state, action)
+    for key in feats.keys():
+      self.w[key] += self.alpha * diff * feats[key]
+
+    
+  def final(self, state):
+    "Called at the end of each game."
+    # call the super-class final method
+    PacmanQAgent.final(self, state)
+    
+    # did we finish training?
+    if self.episodesSoFar == self.numTraining:
+      # you might want to print your weights here for debugging
+      "*** YOUR CODE HERE ***"
+      pass
